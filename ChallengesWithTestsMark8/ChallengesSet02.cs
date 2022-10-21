383b66d27742ff30bbacb4b749ce03e0451148c1
yahel dooley <ydooley@gmail.com>
2022-10-21 14:27:19 -0700

    Completed Week 2 Challenges and Tests

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,52 +8,70 @@ namespace ChallengesWithTestsMark8
 	{
 		public bool CharacterIsALetter(char c)
 		{
-			throw new NotImplementedException();
+			return char.IsLetter(c);
 		}
 
 		public bool CountOfElementsIsEven(string[] vals)
 		{
-			throw new NotImplementedException();
+			return vals.Length % 2 == 0;
 		}
 
 		public bool IsNumberEven(int number)
 		{
-			throw new NotImplementedException();
+			return number % 2 == 0;
 		}
 
 		public bool IsNumberOdd(int num)
 		{
-			throw new NotImplementedException();
+			return num % 2 != 0;
 		}
 
 		public double SumOfMinAndMax(IEnumerable<double> numbers)
 		{
-			throw new NotImplementedException();
+			if (numbers == null || numbers.Count() == 0) return 0;
+			return numbers.Min() + numbers.Max();
 		}
 
 		public int GetLengthOfShortestString(string str1, string str2)
 		{
-			throw new NotImplementedException();
+			if (str1.Length <= str2.Length) return str1.Length;
+			return str2.Length;
 		}
 
 		public int Sum(int[] numbers)
 		{
-			throw new NotImplementedException();
+			if (numbers == null) return 0;
+
+			int sum = 0;
+			foreach (int num in numbers) sum += num;
+			return sum;
 		}
 
 		public int SumEvens(int[] numbers)
 		{
-			throw new NotImplementedException();
+			if (numbers == null) return 0;
+
+			int evenSum = 0;
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				if (numbers[i] % 2 == 0) evenSum += numbers[i];
+			}
+			return evenSum;
 		}
 
 		public bool IsSumOdd(List<int> numbers)
 		{
-			throw new NotImplementedException();
+			if (numbers == null) return false;
+
+			int sum = 0;
+			foreach (int i in numbers) sum += i;
+			return sum % 2 != 0;
 		}
 
 		public long CountOfPositiveOddsBelowNumber(long number)
 		{
-			throw new NotImplementedException();
+			if (number < 0) return 0;
+			return number / 2;
 		}
 	}
 }
